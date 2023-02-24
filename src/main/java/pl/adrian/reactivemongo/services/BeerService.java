9ee2b3981149344c167fe7418e109f5569267cc5
Adiks <apacholak97@interia.pl>
2023-02-24 16:21:17 +0100

    implement crud operations

--- src/main/java/pl/adrian/reactivemongo/services/BeerService.java
+++ src/main/java/pl/adrian/reactivemongo/services/BeerService.java
@@ -1,6 +1,7 @@
 package pl.adrian.reactivemongo.services;
 
 import pl.adrian.reactivemongo.model.BeerDTO;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface BeerService {
@@ -8,4 +9,14 @@ public interface BeerService {
     Mono<BeerDTO> saveBeer(Mono<BeerDTO> beerDTO);
 
     Mono<BeerDTO> getBeerById(String beerId);
+
+    Flux<BeerDTO> listBeers();
+
+    Mono<BeerDTO> saveBeer(BeerDTO beerDTO);
+
+    Mono<BeerDTO> updateBeer(String beerId, BeerDTO beerDTO);
+
+    Mono<BeerDTO> patchBeer(String beerId, BeerDTO beerDTO);
+
+    Mono<Void> deleteBeerById(String beerId);
 }
--- src/main/java/pl/adrian/reactivemongo/services/BeerServiceImpl.java
+++ src/main/java/pl/adrian/reactivemongo/services/BeerServiceImpl.java
@@ -2,9 +2,11 @@ package pl.adrian.reactivemongo.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import pl.adrian.reactivemongo.mappers.BeerMapper;
 import pl.adrian.reactivemongo.model.BeerDTO;
 import pl.adrian.reactivemongo.repositories.BeerRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -24,5 +26,65 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public Mono<BeerDTO> getBeerById(String beerId) {
         return null;
+    }
+
+    @Override
+    public Flux<BeerDTO> listBeers() {
+        return beerRepository.findAll()
+                .map(beerMapper::beerToBeerDto);
+    }
+
+    @Override
+    public Mono<BeerDTO> saveBeer(BeerDTO beerDTO) {
+        return beerRepository.save(beerMapper.beerDtoToBeer(beerDTO))
+                .map(beerMapper::beerToBeerDto);
+    }
+
+    @Override
+    public Mono<BeerDTO> updateBeer(String beerId, BeerDTO beerDTO) {
+        return beerRepository.findById(beerId)
+                .map(foundBeer -> {
+                    foundBeer.setBeerName(beerDTO.getBeerName());
+                    foundBeer.setBeerStyle(beerDTO.getBeerStyle());
+                    foundBeer.setPrice(beerDTO.getPrice());
+                    foundBeer.setUpc(beerDTO.getUpc());
+                    foundBeer.setQuantityOnHand(beerDTO.getQuantityOnHand());
+
+                    return foundBeer;
+                }).flatMap(beerRepository::save)
+                .map(beerMapper::beerToBeerDto);
+    }
+
+    @Override
+    public Mono<BeerDTO> patchBeer(String beerId, BeerDTO beerDTO) {
+        return beerRepository.findById(beerId)
+                .map(foundBeer -> {
+                    if(StringUtils.hasText(beerDTO.getBeerName())){
+                        foundBeer.setBeerName(beerDTO.getBeerName());
+                    }
+
+                    if(StringUtils.hasText(beerDTO.getBeerStyle())){
+                        foundBeer.setBeerStyle(beerDTO.getBeerStyle());
+                    }
+
+                    if(beerDTO.getPrice() != null){
+                        foundBeer.setPrice(beerDTO.getPrice());
+                    }
+
+                    if(StringUtils.hasText(beerDTO.getUpc())){
+                        foundBeer.setUpc(beerDTO.getUpc());
+                    }
+
+                    if(beerDTO.getQuantityOnHand() != null){
+                        foundBeer.setQuantityOnHand(beerDTO.getQuantityOnHand());
+                    }
+                    return foundBeer;
+                }).flatMap(beerRepository::save)
+                .map(beerMapper::beerToBeerDto);
+    }
+
+    @Override
+    public Mono<Void> deleteBeerById(String beerId) {
+        return beerRepository.deleteById(beerId);
     }
 }
