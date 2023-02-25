baaa4ae1219b51ab0f8dc6026ec5afd7803e977c
Adiks <apacholak97@interia.pl>
2023-02-25 09:08:54 +0100

    webflux handler

---
+++ src/main/java/pl/adrian/reactivemongo/web/fn/BeerHandler.java
@@ -0,0 +1,21 @@
+package pl.adrian.reactivemongo.web.fn;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import pl.adrian.reactivemongo.model.BeerDTO;
+import pl.adrian.reactivemongo.services.BeerService;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class BeerHandler {
+
+    private final BeerService beerService;
+
+    public Mono<ServerResponse> listBeers(ServerRequest request) {
+        return ServerResponse.ok()
+                .body(beerService.listBeers(), BeerDTO.class);
+    }
+}
