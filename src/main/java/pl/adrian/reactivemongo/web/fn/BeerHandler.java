2f6169097f97c4f758f4f4b2830d7978f54e0955
Adiks <apacholak97@interia.pl>
2023-02-25 10:06:58 +0100

    update endpoint

--- src/main/java/pl/adrian/reactivemongo/web/fn/BeerHandler.java
+++ src/main/java/pl/adrian/reactivemongo/web/fn/BeerHandler.java
@@ -34,4 +34,11 @@ public class BeerHandler {
                                 .build(beerDTO.getId()))
                         .build());
     }
+
+    public Mono<ServerResponse> updateBeer(ServerRequest request) {
+        return request.bodyToMono(BeerDTO.class)
+                .flatMap(beerDTO -> beerService
+                        .updateBeer(request.pathVariable("beerId"), beerDTO))
+                .flatMap(savedDto -> ServerResponse.noContent().build());
+    }
 }
